e4c2de894a1d0cf879360ac9d5df8250e4af3c4f
lbzhello <i191908577>
2019-09-23 23:30:03 +0800

    update

--- mvc/src/main/java/xyz/lius/mvc/MvcApplication.java
+++ mvc/src/main/java/xyz/lius/mvc/MvcApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaStackApplication {
+public class MvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaStackApplication.class, args);
+		SpringApplication.run(MvcApplication.class, args);
 	}
 
 }
